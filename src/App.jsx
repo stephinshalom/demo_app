dd3581598ba62b37df66b1b2e3f5aea0c05b7e53
Stephin Shalom TJ <stephintintu1@gmail.com>
2025-10-28 15:07:34 +0530

    the new branch2

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>this is brabch1</h2>
+      <h3>it mee</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
